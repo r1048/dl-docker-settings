311a610bc924737c202a062fe6510fe32cdf17e9
r1048 <r1048@naver.com>
2016-08-11 15:46:31 +0900

    Delete defaults.sh

--- install/14.04/defaults.sh
+++
@@ -1,5 +0,0 @@
-sudo apt-get update  
-sudo apt-get upgrade  
-sudo apt-get install build-essential cmake g++ gfortran git pkg-config python-dev software-properties-common wget -y --no-install-recommends
-sudo apt-get autoremove 
-sudo rm -rf /var/lib/apt/lists/*
