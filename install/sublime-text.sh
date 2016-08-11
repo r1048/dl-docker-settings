ea6c9529511fba1e07b3b1e1e801db8bb6574456
r1048 <r1048@naver.com>
2016-08-11 16:14:14 +0900

    Create sublime-text.sh

---
+++ install/sublime-text.sh
@@ -0,0 +1,4 @@
+# see this page: http://www.webupd8.org/2013/07/sublime-text-3-ubuntu-ppa-now-available.html
+sudo add-apt-repository ppa:webupd8team/sublime-text-3
+sudo apt-get update
+sudo apt-get install sublime-text-installer
