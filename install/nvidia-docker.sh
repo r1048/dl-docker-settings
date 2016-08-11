cbef197205b282e9f62ae07fd23954919c967c92
r1048 <r1048@naver.com>
2016-08-11 15:53:10 +0900

    Create nvidia-docker.sh

---
+++ install/nvidia-docker.sh
@@ -0,0 +1,6 @@
+# Install nvidia-docker and nvidia-docker-plugin
+wget -P /tmp https://github.com/NVIDIA/nvidia-docker/releases/download/v1.0.0-rc.3/nvidia-docker_1.0.0.rc.3-1_amd64.deb
+sudo dpkg -i /tmp/nvidia-docker*.deb && rm /tmp/nvidia-docker*.deb
+
+# Test nvidia-smi
+nvidia-docker run --rm nvidia/cuda nvidia-smi
